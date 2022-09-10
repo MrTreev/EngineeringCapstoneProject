73e8562bb8626ea9b893439210cb40f27733fd92
Alec Harbis <58894999+aleco1234@users.noreply.github.com>
2022-09-10 10:56:26 +1000

    Subs code

--- Testing/testingESP/dbTest/test/test.ino
+++ Testing/testingESP/dbTest/test/test.ino
@@ -11,19 +11,46 @@ const char* password = "";
 //AsyncWebServer server(80);
 int i = 0;
 
+Adafruit_AHTX0 aht;
+TwoWire AHTI2C = TwoWire(0);
+
+float voltage = 0;
+float vs = 3.3;
+int Signal = 0;
+bool QS = false;
+int BPM = 0;
+int IBI = 750;
+bool Pulse = false;
+int sampleCounter = 0;
+int lastBeatTime = 0;
+int P = 2048;
+int T = 2048;
+int threshSetting = 3000;
+int thresh = 3000;
+int amp = 410;
+bool firstBeat = true;
+bool secondBeat = false;
+int FadeLevel = 0;
+int delayMs = 20;
+int rate[10];
+
 const char* serverName = "http://your-mindshift.com/post-data.php";
 String apiKeyValue = "tPmAT5Ab3j7F9";
 
 String sensorReading() { // return string of sensor information
-  return String(random(100)); // placeholder value
+  int voltageReading = analogRead(38);
+  voltage = voltageReading * (vs/4095);
+  return String(voltage);
  }
 
 String sensorReading2() { // return string of sensor information
-  return String(random(100)); // placeholder value
+  return String(BPM);
  }
 
 String sensorReading3() { // return string of sensor information
-  return String(random(100)); // placeholder value
+  sensors_event_t humidity, temp;
+  aht.getEvent(&humidity, &temp);
+  return String(temp.temperature);
  }
 void setup(){
   Serial.begin(9600);
@@ -98,6 +125,48 @@ void loop() {
   else {
     Serial.println("WiFi Disconnected");
   }
+
+  sampleCounter = sampleCounter + delayMs; // keeping track of time in ms
+  int N = sampleCounter - lastBeatTime;
+
+  Signal = analogRead(35);
+  Serial.println(Signal);
+
+  if (N > 250 && N < 3000){
+    if (Signal > thresh){
+      Pulse = true;
+      IBI = sampleCounter - lastBeatTime;
+
+      word runningTotal = 0;
+
+      for (int i = 0; i <= 8; i++) {          // shift data in the rate array
+        rate[i] = rate[i + 1];                // and drop the oldest IBI value
+        runningTotal += rate[i];              // add up the 9 oldest IBI values
+      }
+
+      rate[9] = IBI;                          // add the latest IBI to the rate array
+      runningTotal += rate[9];                // add the latest IBI to runningTotal
+      runningTotal /= 10;                     // average the last 10 IBI values
+      BPM = 60000 / runningTotal;             // how many beats can fit into a minute? that's BPM!
+
+      lastBeatTime = sampleCounter;
+    }
+  }
+
+  if (N > 3000) {                          // if 3 seconds go by without a beat
+    thresh = threshSetting;                // set thresh default
+    P = 2048;                               // set P default
+    T = 2048;                               // set T default
+    lastBeatTime = sampleCounter;          // bring the lastBeatTime up to date
+    firstBeat = true;                      // set these to avoid noise
+    secondBeat = false;                    // when we get the heartbeat back
+    Pulse = false;
+    BPM = 0;
+    IBI = 600;                  // 600ms per beat = 100 Beats Per Minute (BPM)
+    Pulse = false;
+    amp = 410;                  // beat amplitude 1/10 of input range.
+  }
+  
   //Send an HTTP POST request every 30 seconds
   delay(30000);
 }
